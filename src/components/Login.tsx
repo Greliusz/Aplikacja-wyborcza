935572b726a8bf31c6c02de7701924d9600a2c9b
Greliusz <53868246+Greliusz@users.noreply.github.com>
2024-08-08 13:26:30 +0200

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -39,35 +39,3 @@ const Login: React.FC = () => {
 };
 
 export default Login;
-
-
-
-
-
-
-// import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import './login.css';
-
-// const Login: React.FC = () => {
-//     const navigate = useNavigate();
-
-//     const handleLogin = () => {
-//         navigate('/login-form'); 
-//     };
-
-//     const handleRegister = () => {
-//         navigate('/register-form'); 
-//     };
-
-//     return (
-//         <div className='wrapper'>
-//             <h1 className="text-2xl font-bold text-center mb-4">Voting App</h1>
-//             <button onClick={handleLogin}>Login</button>
-//             <button onClick={handleRegister}>Sign Up</button>
-//             <button onClick={handleRegister}>Admin</button>
-//         </div>
-//     );
-// };
-
-// export default Login;
